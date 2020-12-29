8a9b547531050d85f044f3ec21ef8bb2c05e598b
rajasekharc <rajasekhar9040@outlook.com>
2020-12-29 13:57:25 +0530

    Delete javasciptQ1.js

--- javasciptQ1.js
+++
@@ -1,7 +0,0 @@
-
-/*given a list of elements = [1, 2, 1] add 41 to it. Assume after adding the number the length of array would remain the same*/
-
-const arr1 = [1,2,1];
-const data1 = (+arr1.join('')) + 41;
-const arr2 = Array.from(data1.toString()).map(e => +e);
-console.log(arr2);
